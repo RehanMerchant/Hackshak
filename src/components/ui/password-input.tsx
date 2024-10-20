7ceb9659e44f3489a53ea718e4312bb75ec228ab
Rehan3000 <skmdrehan64@gmail.com>
2024-10-20 11:52:47 +0530

    vddvd

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -9,7 +9,7 @@ export interface PasswordInput
   }
 
 const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInput>(
-  ({ className, type, ...props }, ref) => {
+  ({ suffix,className, type, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false)
     return (
   <Input type={showPassword ? "text" : "password" } suffix={showPassword ? <LuEye onClick={()=>setShowPassword(false)} className="size-6 text-neutral-500"/> : <LuEyeOff onClick={()=>setShowPassword(true)}  className="size-6 text-neutral-500"/>} className={className} {...props} ref={ref} />
