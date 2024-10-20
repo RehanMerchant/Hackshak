dcd1567bac9cb02f5291c9a46fff75ea8de43fd6
Rehan3000 <skmdrehan64@gmail.com>
2024-10-20 12:08:07 +0530

    sc

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -4,12 +4,10 @@ import { LuEyeOff } from "react-icons/lu";
 import { Input } from "./input"
 
 export interface PasswordInput
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-    suffix?:React.ReactNode;
-  }
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInput>(
-  ({ suffix,className, type, ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false)
     return (
   <Input type={showPassword ? "text" : "password" } suffix={showPassword ? <LuEye onClick={()=>setShowPassword(false)} className="size-6 text-neutral-500"/> : <LuEyeOff onClick={()=>setShowPassword(true)}  className="size-6 text-neutral-500"/>} className={className} {...props} ref={ref} />
