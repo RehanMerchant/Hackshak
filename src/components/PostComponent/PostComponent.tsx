06068441f0a86a7511911fbb278670c2a21ffdc8
Rehan3000 <skmdrehan64@gmail.com>
2024-10-21 17:36:32 +0530

    123

--- src/components/PostComponent/PostComponent.tsx
+++ src/components/PostComponent/PostComponent.tsx
@@ -34,7 +34,7 @@ interface PostInterface{
 const PostComponent = ({name,date,Dp,title,desc,postimage}:PostInterface) => {
   const [like,SetLike] = useState(false)
   return (
-    <div className="w-full border-2 active:bg-secondary cursor-pointer transition-all  rounded-md p-2">
+    <div className="w-full border-2 hover:scale-105 cursor-pointer transition delay-75 duration-200 ease-in-out  rounded-md p-2">
       <div className="py-4 flex items-center  justify-between">
         <div className="flex gap-2">
           <Avatar>
